0a5f5afd4e491034aa92b3b303b67cf7661b42c7
israel <israel.bejarano.a@gmail.com>
2020-04-08 09:55:24 +0200

    Observer y subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 // creacion poco comun de observables
 // const obs$ = Observable.create();
 
+const observer: Observer<any> = {
+    next: value => console.log('siguiente [next]: ', value),
+    error: error => console.warn('error [obs]: ', error),
+    complete: () => console.info('completado [obs')
+};
+
+
+
 const obs$ = new Observable<string>(subs => {
     subs.next('Hola');
+
+    // forzar error
+    const a = undefined;
+    a.nombre = 'Israel';
 
     subs.complete();
 });
 
-obs$.subscribe((resp:string) => {
-    console.log(resp);
-});
+obs$.subscribe(observer);
+
+// obs$.subscribe(
+//     valor => console.log('next: ', valor),
+//     error => console.warn('error: ', error),
+//     () => console.log('completado')
+// );
 
 
 
